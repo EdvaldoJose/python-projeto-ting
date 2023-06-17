ade5d60129c1b1d5f57b97bac7f1cf7b22575736
Edvaldo José <edvaldojose003@gmail.com>
2023-06-17 10:24:13 -0300

    feat: implement file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,39 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    if instance and instance.items:
+        if instance.items[0]["nome_do_arquivo"] == path_file:
+            return None
+
+    info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+
+    instance.enqueue(info)
+    print(str(info), file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos", file=sys.stdout)
+        return None
+
+    file = instance.dequeue()
+    print(
+        f"Arquivo {file['nome_do_arquivo']} removido com sucesso",
+        file=sys.stdout
+        )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+        print(f"{file}", file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
