acb8191486cfedb890e8c5de90057f030dfcc718
Edvaldo José <edvaldojose003@gmail.com>
2023-06-17 09:35:53 -0300

    feat: function txt_import

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return []
+
+    try:
+        with open(path_file, mode="r") as file:
+            lines = file.read().split("\n")
+            return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
